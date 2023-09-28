3d453a1bfe98dd42b0bc3595f0186c6a6f5e42d6
APPLESDD <APPLESDD@DESKTOP-T8BFVIF>
2023-09-29 03:58:43 +0530

    second Commit

---
+++ myproject/src/test/java/com/actiTime/TestScript/CreateCustomer.java
@@ -0,0 +1,15 @@
+package com.actiTime.TestScript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateCustomer {
+	@Test
+	public void test() {
+		Reporter.log("create customer",true);
+	}
+	
+	
+	
+
+}
